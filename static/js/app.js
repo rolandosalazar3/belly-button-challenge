b14a4fd006a98ef9a643a68216eea6a8e4787ba4
rolandosalazar3 <113559238+rolandosalazar3@users.noreply.github.com>
2023-01-09 20:09:35 -0600

    first upload

---
+++ static/js/app.js
@@ -0,0 +1,7 @@
+// Use the D3 library to read in samples.json from the URL.
+const samples = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+// Fetch the JSON data and console log it
+d3.json(samples).then(function(data) {
+  console.log(data);
+});
